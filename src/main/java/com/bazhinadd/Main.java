792b5906231610c92dd8e39c8826f3cd7ddfc105
bazhinadd <bazhinadd@gmail.com>
2025-03-21 15:30:53 +0300

    Update Main.java
    message was changed

--- src/main/java/com/bazhinadd/Main.java
+++ src/main/java/com/bazhinadd/Main.java
@@ -2,6 +2,6 @@ package com.bazhinadd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Goodbye, everybody!");
+        System.out.println("Hi!");
     }
 }
